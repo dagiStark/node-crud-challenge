902a6e7ff36b0c2296768726d56f78cf1a63de49
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-25 14:10:33 +0300

    added person or persons retrieval function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())  // Middleware to parse JSON bodies
 
 let persons = [{
     id: '1',
@@ -8,8 +9,23 @@ let persons = [{
     hobbies: []    
 }] //This is your in memory database
 
+
+
 app.set('db', persons)
 //TODO: Implement crud of person
+
+// to retrieve all persons or a single person by ID
+app.get('/person/:id?', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        const person = persons.find(p => p.id === id);
+        return person ? res.json(person) : res.status(404).json({ error: 'Person not found' });
+    }
+    res.json(persons);
+});
+
+
+
 
 if (require.main === module) {
     app.listen(3000)
